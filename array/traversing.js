6bca03ba79c7734b9735e9dcf12c6ede39d217ca
Utin <durontoalim711@gmail.com>
2021-12-16 18:00:25 +0600

    commit

---
+++ array/traversing.js
@@ -0,0 +1,19 @@
+var arr = [1, 2, 3, 4, 5]
+
+// arr[0]
+// arr[1]
+// arr[2]
+
+// arr[arr.length -1]
+
+for(var i = 0; i < arr.length ; i++){
+    // console.log(arr[i])    //1 2 3 4 5
+
+    arr[i] = arr[i] + 2;
+}
+
+console.log(arr);
+
+var array = 20
+
+var two = 30
